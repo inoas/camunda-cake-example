a4da5fef9759d989858196fec45002d625937a46
Steffen Brand <s.brand@steffenbrand.com>
2017-01-15 13:54:29 +0100

    initial commit - running vacation process and deployment with camunda

---
+++ vacation-request/src/main/java/VacationRequestApplication.java
@@ -0,0 +1,7 @@
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication("Vacation Request App")
+public class VacationRequestApplication extends ServletProcessApplication {
+
+}
